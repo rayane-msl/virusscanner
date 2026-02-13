714a2e2052a49477988640da477a752f9b225746
rayane-msl <155558635+rayane-msl@users.noreply.github.com>
2026-02-13 21:38:33 +0100

    Refactor __init__.py for Utilities package
    Updated package documentation and imports for Utilities.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,26 @@
 """
-Scanners Package
+Utilities Package
 """
 
-from .file_scanner import FileScanner
-from .website_scanner import WebsiteScanner
+from .helpers import (
+    get_file_info,
+    human_readable_size,
+    format_scan_result,
+    validate_file_extension,
+    sanitize_filename,
+    truncate_text,
+    get_risk_level
+)
 
-__all__ = ['FileScanner', 'WebsiteScanner']
+from .stats import stats_tracker
+
+__all__ = [
+    'get_file_info',
+    'human_readable_size',
+    'format_scan_result',
+    'validate_file_extension',
+    'sanitize_filename',
+    'truncate_text',
+    'get_risk_level',
+    'stats_tracker'
+]
